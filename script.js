3d804bd1b3d49aceb62e7bb63b208831730ef78a
Erick <eefeaga2000@gmail.com>
2021-08-04 23:46:51 -0300

    Tabela de rendimento

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ const mesSigla = ['Jan', 'Feb', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set',
 
 
 function calcularRendimento(event) {
+    body.innerHTML = '';
     event.preventDefault();
     let patrimonioEmCotas = Number(document.getElementById("cotasAtual").value);
     let dividendoTotal = 0;
-
+    
     for (let i = 1; i <= anos.value; i++) {
         let divAno = 0;
-
+        
         var ano = document.createElement('div');
         ano.classList.add('ano');
-
+        
         var anoTitle = document.createElement('div');
         anoTitle.classList.add('ano_title');
         anoTitle.textContent = new Date().getFullYear() + i;
-
+        
         ano.appendChild(anoTitle);
-        // body.appendChild(ano);
 
         for (let j = 0; j < 12; j++) {
             patrimonioEmCotas += Number(aporteCotasPorMes.value);
@@ -35,6 +35,7 @@ function calcularRendimento(event) {
 
             var mes = document.createElement('div');
             mes.classList.add('mes');
+            mes.classList.add(j % 2 === 0 ? 'light_color' : 'dark_color');
 
             var mesTitle = document.createElement('div');
             mesTitle.classList.add('mes_title');
@@ -46,28 +47,37 @@ function calcularRendimento(event) {
             mes.appendChild(mesData);
 
             var patrimonioCotasHTML = document.createElement('div');
-            // patrimonioCotasHTML.classList.add('mes_title');
             patrimonioCotasHTML.textContent = 'Cotas: ' + patrimonioEmCotas;
             mesData.appendChild(patrimonioCotasHTML);
 
             var rendeHTML = document.createElement('div');
-            // rendeHTML.classList.add('mes_title');
             rendeHTML.textContent = 'Rende: R$ ' + rende.toFixed(2);
             mesData.appendChild(rendeHTML);
 
             var patrimonioHTML = document.createElement('div');
-            // patrimonioHTML.classList.add('mes_title');
             patrimonioHTML.textContent = 'Patrimonio: R$ ' + (patrimonioEmCotas * valorDaCota.value).toFixed(2);
             mesData.appendChild(patrimonioHTML);
 
-
             ano.appendChild(mes);
-            body.appendChild(ano);
 
-
-            console.log('Ano: ', new Date().getFullYear() + i, 'Mes: ', j, 'Cotas: ', patrimonioEmCotas, 'Rende: $', rende, 'Patrimonio: $', (patrimonioEmCotas * valorDaCota));
         }
-        console.log('Ano: ', new Date().getFullYear() + i, 'Dividendos Ano: ', divAno);
-        console.log('Ano: ', new Date().getFullYear() + i, 'Dividendos total: ', dividendoTotal);
+
+        var rendimentoHTML = document.createElement('div');
+        rendimentoHTML.classList.add('rend');
+        ano.appendChild(rendimentoHTML);
+
+        var rendimentoAno = document.createElement('div');
+        rendimentoAno.textContent = 'Rendimento Ano: R$ ' + divAno.toFixed(2);
+        rendimentoHTML.appendChild(rendimentoAno);
+
+        var rendimentoTotalHTML = document.createElement('div');
+        rendimentoTotalHTML.textContent = 'Rendimento Acumulado: R$ ' + dividendoTotal.toFixed(2);
+        rendimentoHTML.appendChild(rendimentoTotalHTML);
+
+        body.appendChild(ano);
     }
+}
+
+function scrollDown() {
+    window.scrollTo(0, document.body.scrollHeight);
 }
